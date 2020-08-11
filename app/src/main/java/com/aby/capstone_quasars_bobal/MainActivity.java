219a8693bc8ca9dfbb39899de90478038811df80
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-08-11 07:50:16 -0400

    Initial commit

---
+++ app/src/main/java/com/aby/capstone_quasars_bobal/MainActivity.java
@@ -0,0 +1,14 @@
+package com.aby.capstone_quasars_bobal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
