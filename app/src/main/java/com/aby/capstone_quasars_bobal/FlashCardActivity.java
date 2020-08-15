93d6f1da09aee1bf15770f30eb07855e25bb3c7a
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-08-14 20:00:04 -0400

    Added MainActivity

--- app/src/main/java/com/aby/capstone_quasars_bobal/FlashCardActivity.java
+++ app/src/main/java/com/aby/capstone_quasars_bobal/FlashCardActivity.java
@@ -18,7 +18,7 @@ public class FlashCardActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_flashcard);
         findViews();
         loadAnimations();
         changeCameraDistance();
