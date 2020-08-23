4f043cb15ea9d6d82820278d7142aee1573f4c7c
raghavbobal1 <raghavbobal001@gmail.com>
2020-08-22 22:29:12 -0400

    started working on bottom view activity

---
+++ app/src/main/java/com/aby/capstone_quasars_bobal/ui/dashboard/DashboardViewModel.java
@@ -0,0 +1,19 @@
+package com.aby.capstone_quasars_bobal.ui.dashboard;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class DashboardViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public DashboardViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is dashboard fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
