ec1c849dd170f002908cdd743c5820844af0e4c3
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-08-23 14:15:19 -0400

    Added right/wrong buttons to flash card

--- app/src/main/java/com/aby/capstone_quasars_bobal/FlashCardActivity.java
+++ app/src/main/java/com/aby/capstone_quasars_bobal/FlashCardActivity.java
@@ -7,6 +7,8 @@ import android.animation.AnimatorSet;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.json.JSONArray;
@@ -30,6 +32,8 @@ public class FlashCardActivity extends AppCompatActivity {
     String word, meaning;
     ArrayList<String> wordList = new ArrayList<>();
     ArrayList<String> meaningList = new ArrayList<>();
+    LinearLayout btnLayout;
+    Button btnCorrect, btnWrong;
     //HashMap<String, String> wordsMap= new HashMap<>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +41,8 @@ public class FlashCardActivity extends AppCompatActivity {
         setContentView(R.layout.activity_flashcard);
         txtFront = findViewById(R.id.txtFront);
         txtBack = findViewById(R.id.txtBack);
+        btnCorrect = findViewById(R.id.btnCorrect);
+        btnWrong = findViewById(R.id.btnWrong);
         findViews();
         loadAnimations();
         changeCameraDistance();
@@ -45,10 +51,24 @@ public class FlashCardActivity extends AppCompatActivity {
         } catch (JSONException e) {
             e.printStackTrace();
         }
-        int randomNumber = generateRandom();
+        final int randomNumber = generateRandom();
         txtFront.setText(wordList.get(randomNumber));
         //txtBack.setText(wordsMap.get(word));
         txtBack.setText(meaningList.get(randomNumber));
+        btnLayout = findViewById(R.id.btnLayout);
+
+        btnCorrect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int rand2 = generateRandom();
+                findViews();
+                loadAnimations();
+                changeCameraDistance();
+                flipCard();
+                txtFront.setText(wordList.get(rand2));
+                txtBack.setText(meaningList.get(rand2));
+            }
+        });
     }
 
     private void changeCameraDistance() {
@@ -75,6 +95,7 @@ public class FlashCardActivity extends AppCompatActivity {
             mSetRightOut.start();
             mSetLeftIn.start();
             mIsBackVisible = true;
+            btnLayout.setVisibility(View.VISIBLE);
         } else {
             mSetRightOut.setTarget(mCardBackLayout);
             mSetLeftIn.setTarget(mCardFrontLayout);
@@ -117,11 +138,28 @@ public class FlashCardActivity extends AppCompatActivity {
     }
     public int generateRandom()
     {
-        int max = 5;
+        int max = 4;
         int min = 0;
         Random rand = new Random();
         int randomNum = rand.nextInt((max - min) + 1) + min;
         return randomNum;
+    }
+
+    public void flipCard() {
+        if (!mIsBackVisible) {
+            mSetRightOut.setTarget(mCardFrontLayout);
+            mSetLeftIn.setTarget(mCardBackLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            mIsBackVisible = true;
+            btnLayout.setVisibility(View.VISIBLE);
+        } else {
+            mSetRightOut.setTarget(mCardBackLayout);
+            mSetLeftIn.setTarget(mCardFrontLayout);
+            mSetRightOut.start();
+            mSetLeftIn.start();
+            mIsBackVisible = false;
+        }
     }
 }
 
