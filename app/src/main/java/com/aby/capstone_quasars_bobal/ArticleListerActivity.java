3fdab83a5f344f38066b5969f749122febb7ff04
prana-magar <Prakashrana9847067614@gmail.com>
2020-08-24 22:04:23 -0400

    added search feature in article lister

--- app/src/main/java/com/aby/capstone_quasars_bobal/ArticleListerActivity.java
+++ app/src/main/java/com/aby/capstone_quasars_bobal/ArticleListerActivity.java
@@ -1,10 +1,13 @@
 package com.aby.capstone_quasars_bobal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.SearchView;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.aby.capstone_quasars_bobal.adapter.ArticleAdapter;
 import com.aby.capstone_quasars_bobal.model.Article;
@@ -46,9 +49,78 @@ public class ArticleListerActivity extends AppCompatActivity {
 
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         articles = new ArrayList<>();
-
         requestQueue = Volley.newRequestQueue(this);
+
+
+        articleAdapter = new ArticleAdapter(ArticleListerActivity.this,articles, requestQueue);
+        recyclerView.setAdapter(articleAdapter);
+
         parseArticle();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.article_lister_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.article_search);
+        androidx.appcompat.widget.SearchView searchView = (androidx.appcompat.widget.SearchView) menuItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+
+
+                if(newText.isEmpty()){
+                    newText= "*:*";
+                }
+                String url = "http://142.93.156.57:8983/solr/article_search/select?rows=20&q="+newText;
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONObject jsonObject = response.getJSONObject("response");
+
+                                    JSONArray jsonArray = jsonObject.getJSONArray("docs");
+
+                                    articles.clear();
+                                    for(int i =0 ; i<jsonArray.length(); i++){
+                                        JSONObject doc = jsonArray.getJSONObject(i);
+                                        String title = doc.getString("title");
+                                        String imageUrl = doc.getString("url_to_image");
+                                        String sourceUrl = doc.getString("url");
+
+                                        articles.add(new Article(imageUrl,title,sourceUrl));
+                                    }
+
+                                    articleAdapter = new ArticleAdapter(ArticleListerActivity.this,articles, requestQueue);
+                                    recyclerView.setAdapter(articleAdapter);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        },
+
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                            }
+                        });
+
+                requestQueue.add(jsonObjectRequest);
+
+
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 
     private void parseArticle(){
@@ -72,7 +144,7 @@ public class ArticleListerActivity extends AppCompatActivity {
                                 articles.add(new Article(imageUrl,title,sourceUrl));
                             }
 
-                            articleAdapter = new ArticleAdapter(ArticleListerActivity.this,articles);
+                            articleAdapter = new ArticleAdapter(ArticleListerActivity.this,articles, requestQueue);
                             recyclerView.setAdapter(articleAdapter);
 
                         } catch (JSONException e) {
--- app/src/main/java/com/aby/capstone_quasars_bobal/adapter/ArticleAdapter.java
+++ app/src/main/java/com/aby/capstone_quasars_bobal/adapter/ArticleAdapter.java
@@ -4,6 +4,8 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,20 +15,36 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.aby.capstone_quasars_bobal.ArticleListerActivity;
 import com.aby.capstone_quasars_bobal.R;
 import com.aby.capstone_quasars_bobal.model.Article;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 import com.squareup.picasso.Picasso;
 
-import java.util.ArrayList;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
-public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder> {
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ArticleViewHolder> implements Filterable {
 
 
     private Context context;
     private ArrayList<Article> articles;
+    private ArrayList<Article> articlesAll;
+    private RequestQueue requestQueue;
 
 
-    public ArticleAdapter(Context context, ArrayList<Article> articles){
+    public ArticleAdapter(Context context, ArrayList<Article> articles, RequestQueue requestQueue){
         this.context = context;
         this.articles = articles;
+        this.articlesAll = new ArrayList<>(articles);
+        this.requestQueue = requestQueue;
     }
 
     @NonNull
@@ -49,6 +67,119 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ArticleV
     public int getItemCount() {
         return articles.size();
     }
+
+    @Override
+    public Filter getFilter() {
+        return filter;
+    }
+
+    private void parseArticle(){
+        String url = "http://142.93.156.57:8983/solr/article_search/select?q=entrenched";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            JSONObject jsonObject = response.getJSONObject("response");
+
+                            JSONArray jsonArray = jsonObject.getJSONArray("docs");
+
+                            for(int i =0 ; i<jsonArray.length(); i++){
+                                JSONObject doc = jsonArray.getJSONObject(i);
+                                String title = doc.getString("title");
+                                String imageUrl = doc.getString("url_to_image");
+                                String sourceUrl = doc.getString("url");
+
+                                articles.add(new Article(imageUrl,title,sourceUrl));
+                            }
+
+
+
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                },
+
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        error.printStackTrace();
+                    }
+                });
+
+        requestQueue.add(jsonObjectRequest);
+    }
+
+
+
+    Filter filter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+
+            List<Article> filteredList = new ArrayList<>();
+            if(charSequence.toString().isEmpty()){
+                filteredList.addAll(articlesAll);
+            }
+            else{
+                // voley call
+                String url = "http://142.93.156.57:8983/solr/article_search/select?q=*:*";
+
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                try {
+                                    JSONObject jsonObject = response.getJSONObject("response");
+
+                                    JSONArray jsonArray = jsonObject.getJSONArray("docs");
+
+                                    for(int i =0 ; i<jsonArray.length(); i++){
+                                        JSONObject doc = jsonArray.getJSONObject(i);
+                                        String title = doc.getString("title");
+                                        String imageUrl = doc.getString("url_to_image");
+                                        String sourceUrl = doc.getString("url");
+
+                                        articles.add(new Article(imageUrl,title,sourceUrl));
+                                    }
+                                    System.out.println("response = article count:" + articles.size());
+                                    filteredList.addAll(articles);
+
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+
+                            }
+                        },
+
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                error.printStackTrace();
+                            }
+                        });
+
+                requestQueue.add(jsonObjectRequest);
+
+                // end call
+
+            }
+
+            FilterResults filterResults = new FilterResults();
+            filterResults.values = filteredList;
+            return filterResults;
+
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            articles.clear();
+            articles.addAll((Collection<? extends Article>) filterResults.values);
+            notifyDataSetChanged();
+        }
+    };
 
     public class ArticleViewHolder extends RecyclerView.ViewHolder{
 
