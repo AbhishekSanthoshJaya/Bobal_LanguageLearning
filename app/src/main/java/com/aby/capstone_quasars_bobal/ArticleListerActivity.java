c11ccfdabac12ffe2b113c875ab15439308f1eaa
prana-magar <Prakashrana9847067614@gmail.com>
2020-08-28 05:40:14 -0400

    cardview in test listeer

--- app/src/main/java/com/aby/capstone_quasars_bobal/ArticleListerActivity.java
+++ app/src/main/java/com/aby/capstone_quasars_bobal/ArticleListerActivity.java
@@ -42,6 +42,8 @@ public class             ArticleListerActivity extends AppCompatActivity impleme
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_article_lister);
+        getSupportActionBar().setTitle("Articles");
+
 
         ButterKnife.bind(this);
 
