96282281d7abc8dd4afc2225b706949025ef8085
raghavbobal1 <raghavbobal001@gmail.com>
2020-08-28 04:48:38 -0400

    designed first onboarding fragment

---
+++ app/src/main/java/com/aby/capstone_quasars_bobal/onboarding/ViewPagerFragment.kt
@@ -0,0 +1,22 @@
+package com.aby.capstone_quasars_bobal.onboarding
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.aby.capstone_quasars_bobal.R
+
+class ViewPagerFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val  view = inflater.inflate(R.layout.fragment_view_pager, container, false)
+
+        val fragmentlist = arrayListOf<Fragment>()
+
+    }
+
+}
